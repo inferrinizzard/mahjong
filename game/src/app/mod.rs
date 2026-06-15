06116cfb61f590c778bb9e0c60076ba183d6ab96
inferrinizzard <inferrinizzard@gmail.com>
2026-06-14 20:48:25 -0700

    chore: skeleton state structs

--- game/src/app/mod.rs
+++ game/src/app/mod.rs
@@ -1,1 +1,4 @@
+pub mod game;
 pub mod root;
+pub mod server;
+pub mod settings;
--- game/src/app/root.rs
+++ game/src/app/root.rs
@@ -1,1 +1,22 @@
-pub struct AppRoot {}
+use crate::{
+    app::{game::Game, server::ServerRoot, settings::Settings},
+    ui::root::RenderRoot,
+};
+
+pub struct AppRoot {
+    pub render: RenderRoot,
+    pub settings: Settings,
+    pub game: Option<Game>,
+    pub server: ServerRoot,
+}
+
+impl AppRoot {
+    pub fn init() -> AppRoot {
+        Self {
+            render: RenderRoot::default(),
+            game: None,
+            settings: Settings::default(),
+            server: ServerRoot::default(),
+        }
+    }
+}
